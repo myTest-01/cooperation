7d443efa702f7c7c964f985db989eed825ac9d32
maliang.huang <maliang.huang@gogen.com.cn>
2018-07-12 15:34:04 +0800

    develop分支添加一个pojo类

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	private  String name4;
 	private  String age4;
     private  String sex4;
+    private  String grade4;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("hello:springboot");
