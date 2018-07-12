483e94b058cac61db8d152eaa3e5ab8475a114ff
maliang.huang <maliang.huang@gogen.com.cn>
2018-07-12 15:09:48 +0800

    develop分支添加一个pojo类

---
+++ src/main/java/com/example/demo/NewApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class NewApplication {
+    private String name;
+    private Integer  age;
+    private String  grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
