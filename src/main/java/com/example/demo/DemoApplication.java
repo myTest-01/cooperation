a94aa88f80c290089d9ee08c8b7e4ae1bff34170
maliang.huang <maliang.huang@gogen.com.cn>
2018-07-12 14:00:04 +0800

    增加成员变量name1,age1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	private  String name1;
+	private  String age1;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("hello:springboot");
 	}
 }
