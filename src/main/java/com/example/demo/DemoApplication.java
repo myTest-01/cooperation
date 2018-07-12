bcdf9c2063239be1c6ec11aa6e34bb17c755a219
maliang.huang <maliang.huang@gogen.com.cn>
2018-07-12 14:03:52 +0800

    修改冲突演示

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	private  String name1;
-	private  String age1;
+	private  String name2;
+	private  String age2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
